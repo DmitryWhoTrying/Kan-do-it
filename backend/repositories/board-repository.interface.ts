437d5ec44def8203eb863be2bd9e28f45820f306
dmitry the developer <dmitryredin321@gmail.com>
2026-03-08 14:03:58 +0300

    merged branches

---
+++ backend/repositories/board-repository.interface.ts
@@ -0,0 +1,13 @@
+import {Board} from "../../shared/types"
+
+export interface IBoardRepository{
+    findById(id: number): Promise<Board | null>;
+    findByName(name: string): Promise<Board | null>;
+    findByUser(userId: number): Promise<Board[] | null>;
+    findByOwner(ownerId: number): Promise<Board[] | null>;
+
+    create(board: Omit<Board, 'id' | 'createdAt'>): Promise<Board>;
+    update(id: string, data: Partial<Board>): Promise<Board | null>;
+    delete(id: string): Promise<boolean>;
+    findAll(): Promise<Board[]>;
+}
